902dd6a3260dfd462f1699f2e516bca2adef5261
Jesus Araujo <neo.jando@gmail.com>
2023-06-30 21:31:01 +0000

    Added new entry and entries view to code journal.

---
+++ client/src/Entries.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function Entries() {
+  return (
+    <div class="container" data-view="entries">
+      <div class="row">
+        <div class="column-full d-flex justify-between align-center">
+          <h1>Entries</h1>
+          <h3>
+            <button id="formLink" class="white-text form-link" href="#">
+              NEW
+            </button>
+          </h3>
+        </div>
+      </div>
+      <div class="row">
+        <div class="column-full">
+          <ul class="entry-ul" id="entryUl"></ul>
+        </div>
+      </div>
+    </div>
+  );
+}
