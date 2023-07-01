ec391cbe9780bfb509972334ba4e7a830a8f57e0
Jesus Araujo <neo.jando@gmail.com>
2023-07-01 00:00:53 +0000

    Attempted local storage in React.

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,35 @@ import './layout.css';
 import './styles.css';
 import EntryForm from './EntryForm';
 import EntryList from './EntryList';
-
 import Navbar from './Navbar';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  // const [view, setView] = useState('entries');
+  const [entries, setEntries] = useState([]);
+
+  useEffect(() => {
+    const storedEntries = localStorage.getItem('entries');
+    if (setEntries) {
+      setEntries(JSON.parse(storedEntries));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('entries', JSON.stringify(entries));
+  }, [entries]);
+
+  const handleSaveEntry = (formValues) => {
+    const newEntry = { ...formValues, entryID: entries.length.length + 1 };
+    setEntries([newEntry, ...entries]);
+    // const updatedEntries = entries.map((entry) => {...formValues, entryID: entry.entryID} : entry)
+    // setEntries(updateEntries);
+  };
+
   return (
     <div className="App">
       <Navbar />
-      <EntryForm />
+      <EntryForm onSave={handleSaveEntry} />
       <EntryList />
     </div>
   );
